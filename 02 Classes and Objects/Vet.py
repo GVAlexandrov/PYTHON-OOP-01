3c43aacad18cae78f5b78ffb275156cabdfffc7b
GVAlexandrov <80593140+GVAlexandrov@users.noreply.github.com>
2023-11-16 23:06:58 +0200

    UPDATE Vet.py

--- 02 Classes and Objects/Vet.py
+++ 02 Classes and Objects/Vet.py
@@ -1,23 +1,24 @@
 class Vet:
     space = 5
-    animals_total = []
+    animals = []
+
     def __init__(self, name):
         self.name = name
         self.animals = []
 
     def register_animal(self, animal_name):
-        if len(Vet.animals_total) < Vet.space:
-            Vet.animals_total.append(animal_name)
-            self.animals.append(animal_name)
-            return f"{animal_name} registered in the clinic"
-        return "Not enough space"
+        if len(self.animals) >= Vet.space:
+            return "Not enough space"
+        Vet.animals.append(animal_name)
+        self.animals.append(animal_name)
+        return f"{animal_name} registered in the clinic"
 
     def unregister_animal(self, animal_name):
-        if animal_name in self.animals:
-            Vet.animals_total.remove(animal_name)
-            self.animals.remove(animal_name)
-            return f"{animal_name} unregistered successfully"
-        return f"{animal_name} not in the clinic"
+        if animal_name not in self.animals:
+            return f"{animal_name} not in the clinic"
+        Vet.animals.remove(animal_name)
+        self.animals.remove(animal_name)
+        return f"{animal_name} unregistered successfully"
 
     def info(self):
         return f"{self.name} has {len(self.animals)} animals. {Vet.space - len(Vet.animals_total)} space left in " \
